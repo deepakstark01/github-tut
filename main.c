b31749e85dc7d080ae4a0d49e95a23b0a6e40872
Deepak <60574055+deepakstark01@users.noreply.github.com>
2024-02-04 17:57:16 +0000

    react js

--- main.c
+++ main.c
@@ -4,8 +4,10 @@ void main()
     int a = 4,b = 5,c;
     c=a+b;
 
-    printf("Sum of a and b is %d",c); // hey this is a comment
+    printf("Sum of a and b is %d\n",c); // hey this is a comment
 
+
+    printf("hello i am rahman react js developer")
 
 }
 
